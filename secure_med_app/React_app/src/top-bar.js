682e158753f0be5198a69965320c1c1c98750f2c
JAIL10276 <jai5143@psu.edu>
2025-11-30 18:30:21 -0500

    Fixed frontend addresse

--- secure_med_app/React_app/src/top-bar.js
+++ secure_med_app/React_app/src/top-bar.js
@@ -6,7 +6,7 @@ export default function TopBar(){
             <span className="login-text" style={{fontSize:"12px"}}>sign in</span>
             <img 
                 className="lab-coat-icon" 
-                src="./Assets/lab-coat.svg"
+                src="/Assets/lab-coat.svg"
                 alt = "Lab coat"
                 />
 
